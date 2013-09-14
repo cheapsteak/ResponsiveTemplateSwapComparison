279e9a6093e74bb17e8085ea14d83f2d4474bc18
chang wang <garnwraly@gmail.com>
2013-09-15 01:32:41 +0800

    updated styles

--- js/angular.js
+++ js/angular.js
@@ -1,5 +1,5 @@
 var repos = [{"name":"bootstrap","owner":{"html_url":"https://github.com/twbs","login":"twbs"},"html_url":"https://github.com/twbs/bootstrap","size":"64893","forks":"20018","open_issues":"159","watchers":"58229"},{"name":"node","owner":{"html_url":"https://github.com/joyent","login":"joyent"},"html_url":"https://github.com/joyent/node","size":"380079","forks":"4731","open_issues":"633","watchers":"24526"},{"name":"jquery","owner":{"html_url":"https://github.com/jquery","login":"jquery"},"html_url":"https://github.com/jquery/jquery","size":"22400","forks":"4899","open_issues":"7","watchers":"23630"},{"name":"html5-boilerplate","owner":{"html_url":"https://github.com/h5bp","login":"h5bp"},"html_url":"https://github.com/h5bp/html5-boilerplate","size":"73410","forks":"5422","open_issues":"8","watchers":"22283"},{"name":"rails","owner":{"html_url":"https://github.com/rails","login":"rails"},"html_url":"https://github.com/rails/rails","size":"220428","forks":"6557","open_issues":"741","watchers":"19587"}];
-//normally you'd use `app.factory` to create a service to make a request
+//normally you'd create a service here and make a request from the controller
 
 var app = angular.module("gitRepos", ['ngResource', 'ngAnimate']);
 
--- js/backbone.js
+++ js/backbone.js
@@ -1,17 +1,15 @@
 var repos = [{"name":"bootstrap","owner":{"html_url":"https://github.com/twbs","login":"twbs"},"html_url":"https://github.com/twbs/bootstrap","size":"64893","forks":"20018","open_issues":"159","watchers":"58229"},{"name":"node","owner":{"html_url":"https://github.com/joyent","login":"joyent"},"html_url":"https://github.com/joyent/node","size":"380079","forks":"4731","open_issues":"633","watchers":"24526"},{"name":"jquery","owner":{"html_url":"https://github.com/jquery","login":"jquery"},"html_url":"https://github.com/jquery/jquery","size":"22400","forks":"4899","open_issues":"7","watchers":"23630"},{"name":"html5-boilerplate","owner":{"html_url":"https://github.com/h5bp","login":"h5bp"},"html_url":"https://github.com/h5bp/html5-boilerplate","size":"73410","forks":"5422","open_issues":"8","watchers":"22283"},{"name":"rails","owner":{"html_url":"https://github.com/rails","login":"rails"},"html_url":"https://github.com/rails/rails","size":"220428","forks":"6557","open_issues":"741","watchers":"19587"}];
-
-var tableTemplate = Handlebars.compile($('#table-template').html()),
-    listTemplate = Handlebars.compile($('#list-template').html());
+// Normally a Backbone Model would be used to fetch data
 
 var ResponsiveView = Backbone.View.extend({
-    initialize: function () {
+    initialize: function (options) {
         var self = this;
         if (window.matchMedia) {
             var mqlHandler = function (mql) {
             if (mql.matches) {
-                self.template = tableTemplate;
+                self.template = options.tableTemplate;
             } else {
-                self.template = listTemplate;
+                self.template = options.cardTemplate;
             }
                 self.render();
             };
@@ -28,6 +26,12 @@ var ResponsiveView = Backbone.View.extend({
     },
 });
 
-new ResponsiveView({
-    el: '#gitRepos'
+$(function () {
+    var tableTemplate = Handlebars.compile($('#table-template').html()),
+        cardTemplate = Handlebars.compile($('#card-template').html());
+    new ResponsiveView({
+        el: '#gitRepos',
+        tableTemplate: tableTemplate,
+        cardTemplate: cardTemplate
+    });
 });
